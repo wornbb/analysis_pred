3f205cbd357b60948cffd8f61f2e836e5b1fad3e
Yi Shen <shen1@ualberta.ca>
2019-02-26 11:14:28 -0500

    fixed a bug in get_batch_data which gives large dim

--- mscripts/get_batch_data.m
+++ mscripts/get_batch_data.m
@@ -1,13 +1,14 @@
 function batch_data = get_batch_data(name, iter, batch_size)
     root = "/data/yi/vioPred/data";
     fname = name + ".gridIR";
-    f = fullfile(root, fname);
+    f = fullfile(root, name,  fname);
     fid = fopen(f);
     % load the first line to get the size of matrix
     % so we can initialize batch_data 
     tline = fgetl(fid);
     matrix1D = str2num(tline);
-    batch_data = zeros(length(tline), batch_size);
+    batch_data = zeros(length(matrix1D), batch_size);
+    frewind(fid);
    % skip unwanted cycles
     skip = (iter - 1) * batch_size;
     for k = 1:skip
@@ -18,7 +19,8 @@ function batch_data = get_batch_data(name, iter, batch_size)
     for k = (1 + skip):target
         tline = fgetl(fid);
         matrix1D = str2num(tline);
-        batch_data(:,k) = matrix1D;
+        batch_data(:,k) = matrix1D';
     end
         
+    fclose(fid);
 end
