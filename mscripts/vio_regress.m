66ee1574195e4a083202118e42dc628ba2c27dfa
Yi Shen <yi.shen4@mail.mcgill.ca>
2019-02-26 11:23:35 -0500

    a testing lasso experiment

--- mscripts/vio_regress.m
+++ mscripts/vio_regress.m
@@ -1,12 +1,14 @@
+tic
 % define violation
 Vth = 0.8;
 vioP = 5;
 
 % load data
-dim = 100; % dimension needs to be even number
-volt_grid = magic(dim);
+exp = "Yaswan2c";
+batch_size = 5000;
+data = get_batch_data(exp,1, batch_size);
 % preprocessing
-vg_normed = normalize(volt_grid);
+vg_normed = normalize(data);
 
 % take odd index values as input
 x = vg_normed(1:2:end,:);
@@ -14,3 +16,4 @@ x = vg_normed(1:2:end,:);
 y = vg_normed(2:2:end,:);
 b = batch_lasso_l2(x,y);
 
+toc
