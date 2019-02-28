512d782d0551b8cd88254ad328464b25d6e4ae4f
Yi Shen <yi.shen4@mail.mcgill.ca>
2019-02-28 04:19:12 -0500

    rename variable

--- mscripts/get_batch_data.m
+++ mscripts/get_batch_data.m
@@ -11,7 +11,7 @@ function batch_data = get_batch_data(name, iter, batch_size)
     frewind(fid);
    % skip unwanted cycles
     offset = (iter - 1) * batch_size;
-    for k = 1:skip
+    for k = 1:offset
         fgetl(fid);
     end
     % real loading
