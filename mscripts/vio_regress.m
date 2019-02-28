37bd45e194c8034d0ec203751958e91955f43722
Yi Shen <yi.shen4@mail.mcgill.ca>
2019-02-28 03:37:57 -0500

    fixes

--- mscripts/vio_regress.m
+++ mscripts/vio_regress.m
@@ -19,11 +19,11 @@ b = batch_lasso_l2(x,y,15);
 b_norm = norms(b);
 best_sensor = max(b_norm);
 good_sensor = (b_norm >= 0.8 * best_sensor);
-[a ,c] = my_ols(X(good_sensor,:),Y);
+[a ,c] = my_ols(X(good_sensor,:), Y);
 toc
 
 % testing 
 test_data = get_batch_data(exp,10, batch_size);
 Xt = test_data(1:2:end,:);
 Yt = test_data(1:2*50:end,:);
-norm(Yt-Xt*a - E)
+norm(Yt-Xt*a - c)
