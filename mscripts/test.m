a67b3a1de6e4aed058d075dcf3fa1884ba506597
Yi Shen <yi.shen4@mail.mcgill.ca>
2019-03-13 22:12:43 -0400

    continue testing

---
+++ mscripts/test.m
@@ -0,0 +1,29 @@
+n = length(data);
+x_size = size(data(1).variable.xtest) - [0, shift];
+y_size = size(data(1).variable.ytest) - [0, shift];
+xtest = zeros([x_size, shift]);
+ytest = zeros([y_size, shift]);
+for j = 1:n
+    for k = 1:shift
+        xtest(:,:,k) = data(j).variable.xtest(:, 1 + k: end - shift + k);
+        ytest(:,:,k) = data(j).variable.ytest(:, 1 + k: end - shift + k);
+        x(:,:,k) = data(j).variable.x(:, 1 + k: end - shift + k);
+        y(:,:,k) = data(j).variable.y(:, 1 + k: end - shift + k);
+    end
+    data(j).variable.xtest = xtest;
+    data(j).variable.ytest = ytest;
+    data(j).variable.x = x;
+    data(j).variable.y = y;
+end
+
+for k = 1:n
+    As = cat(3, solution(:).sets.A);
+    Axs = batch_mtimes(As, data(k).variable.x);
+    cvx_begin
+        variable weight(shift)
+        minimize norm( data(k).variable.y -  Axs .* weight)
+    cvx_end
+    solution(k).weight = weight;
+end
+
+solution = test_sol(data, solution);
