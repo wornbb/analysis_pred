aa801c001d5a83d8e355fb07d7526ce85986dd87
wornbb <wornbb@gmail.com>
2019-07-10 22:29:55 -0400

    added save to csv

--- pyscripts/testLSTM.py
+++ pyscripts/testLSTM.py
@@ -8,7 +8,7 @@ import pickle
 import tensorflow as tf
 from loading import read_violation
 from keras import regularizers
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, CSVLogger
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
@@ -23,40 +23,34 @@ x_train = np.expand_dims(x_train, axis=2)
 x_test = scaler.fit_transform(x_test[:,:,0])
 x_test = np.expand_dims(x_test, axis=2)
 
-class_weight = {0: 1,1:1}
-model = Sequential()
-print(len(y_test), sum(y_test))
-#model.add(LSTM(128, kernel_initializer='random_uniform', bias_initializer='zeros', return_sequences=True, kernel_regularizer=regularizers.l2(0.01)))
-#model.add(LSTM(500, kernel_initializer='random_uniform', bias_initializer='zeros', return_sequences=True))
+csv_logger = CSVLogger('training.log',append=True)
 
-#model.add(LSTM(500, kernel_initializer='random_uniform', bias_initializer='zeros', return_sequences=True))
-n = 16
-model.add(LSTM(n, kernel_initializer='random_uniform', bias_initializer='zeros', return_sequences=True))
-model.add(LSTM(n, kernel_initializer='random_uniform', bias_initializer='zeros', return_sequences=True))
-#model.add(LSTM(n, kernel_initializer='random_uniform', bias_initializer='zeros', return_sequences=True))
+for n in range(20,50):
+    model = Sequential()
+    print(len(y_test), sum(y_test))
+    model.add(LSTM(n, kernel_initializer='random_uniform', bias_initializer='zeros', return_sequences=True))
+    model.add(LSTM(n, kernel_initializer='random_uniform', bias_initializer='zeros', return_sequences=True))
+    model.add(LSTM(n, kernel_initializer='random_uniform', bias_initializer='zeros', return_sequences=True))
+    model.add(LSTM(n, kernel_initializer='random_uniform', bias_initializer='zeros'))
+    #model.add(Bidirectional(LSTM(128, input_shape=(timestep,9), kernel_initializer='random_uniform', bias_initializer='zeros')))
+    model.add(Dense(32, activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros'))
+    model.add(Dropout(0.15))
+    model.add(Dense(16, activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros'))
+    model.add(Dropout(0.15))
+    model.add(Dense(2, activation='softmax', kernel_initializer='random_uniform', bias_initializer='zeros'))
 
-model.add(LSTM(n, kernel_initializer='random_uniform', bias_initializer='zeros'))
-#model.add(Bidirectional(LSTM(128, input_shape=(timestep,9), kernel_initializer='random_uniform', bias_initializer='zeros')))
-#model.add(Dropout(0.2))
-#model.add(Dense(50, activation='tanh'))
-model.add(Dense(32, activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros'))
-model.add(Dropout(0.1))
-model.add(Dense(16, activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros'))
-model.add(Dropout(0.1))
-model.add(Dense(2, activation='softmax', kernel_initializer='random_uniform', bias_initializer='zeros'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
-
-print('Train...')
-filepath = "model.{epoch:02d}-{val_loss:.3f}.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, mode='min')
-batch_size = 5
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          validation_data=(x_test[::1000,:,:],y_test[::1000]),
-          epochs=8,
-          callbacks=[checkpoint],
-          verbose=1)
+    print('Train...')
+    filepath = "model.{epoch:02d}-{val_loss:.3f}.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor='val_categorical_accuracy',save_best_only=True, verbose=1, mode='max')
+    batch_size = 5
+    model.fit(x_train[::100,:,:], y_train[::100],
+            batch_size=batch_size,
+            validation_data=(x_test[::1000,:,:],y_test[::1000]),
+            epochs=25,
+            callbacks=[checkpoint, csv_logger],
+            verbose=1)
 # model.fit(x_train[::100,:,:], y_train[::100],
 #           batch_size=batch_size,
 #           validation_data=(x_test[::1000,:,:],y_test[::1000]),
@@ -65,7 +59,7 @@ model.fit(x_train, y_train,
 #pickle.dump( model, open( "single_sensor_lstm10.p", "wb" ) )
 
 
-scores = model.evaluate(x_test, y_test, verbose=0)
-print(sum(y_test)/len(y_train))
-print("Accuracy: %.2f%%" % (scores[1]*100))
+# scores = model.evaluate(x_test, y_test, verbose=0)
+# print(sum(y_test)/len(y_train))
+# print("Accuracy: %.2f%%" % (scores[1]*100))
 
