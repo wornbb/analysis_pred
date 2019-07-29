edb6e7a8c5618b58cff3b4616a34b9a7fc833f1b
wornbb <wornbb@gmail.com>
2019-07-29 09:41:49 -0400

    updated interface

--- pyscripts/generate_grid_data_with_label.py
+++ pyscripts/generate_grid_data_with_label.py
@@ -6,12 +6,13 @@ from loading import generate_prediction_data
 from tensorflow.keras.utils import to_categorical
 import os
 if os.name == 'nt':
+      name = "Yaswan2c.gridIR"
       fname = "F:\\Yaswan2c\\Yaswan2c.gridIR"
 else:
-      name = input("Give the simulation name")
+      name = input("Give the simulation name: ")
       fname = "/data/yi/voltVio/analysis/raw/" + name + ".gridIR"
       print("loading " + fname)
-(data, tag) = generate_prediction_data(fname, selected_sensor='all',trace=39, ref=1, pred_str=5, thres=4, lines_to_read=10000)
+(data, tag) = generate_prediction_data(fname, selected_sensor='all',trace=39, ref=1, pred_str=5, thres=4)
 print("Loading complete")
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler(feature_range=(-0.5, 0.5))
@@ -20,6 +21,8 @@ for instance in range(data.shape[1]):
       data[:,instance,:] = scaler.fit_transform(piece)
 data = np.expand_dims(data, axis=-1)
 
-with h5py.File("old_balanced_grid_sensor.h5","w") as hf:
+save_fname = "balanced_gird_sensor." + name + ".h5"
+print("Saving to " + save_fname)
+with h5py.File(save_fname,"w") as hf:
       hf.create_dataset("data", data=data, dtype = 'float32')
       hf.create_dataset("tag", data=tag, dtype = 'float32')
