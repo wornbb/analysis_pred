791d7fcbd68c154afb0e6ba8337e1c26d185357b
wornbb <wornbb@gmail.com>
2019-08-06 14:58:33 -0400

    no message

--- pyscripts/generate_grid_data_with_label.py
+++ pyscripts/generate_grid_data_with_label.py
@@ -9,6 +9,8 @@ if os.name == 'nt':
       name = "Yaswan2c_desktop"
       fname = "F:\\Yaswan2c\\Yaswan2c.gridIR"
       f_list = [fname]
+      lstm_save = "lstm_2c.h5"
+      net_save = "VoltNet_2c.h5"
 else:
       #name = input("Give the simulation name: ")
       fname = "/data/yi/voltVio/analysis/raw/" + name + ".gridIR"
@@ -19,14 +21,17 @@ else:
       "/data/yi/voltVio/analysis/raw/" + "freqmine2c"+ ".gridIR",
       "/data/yi/voltVio/analysis/raw/" + "facesim2c"+ ".gridIR",
       ]
+      dump = "/media/yi/yi_final_resort/"
+      lstm_save = dump + "lstm_2c.h5"
+      net_save = dump + "VoltNet_2c.h5"
 grid_size = 5776
 lstm_samples = 0
 grid_samples = 0
 
-with h5py.File("lstm_2c.h5", 'w') as lstmF:
+with h5py.File(lstm_save, 'w') as lstmF:
       lstmX = lstmF.create_dataset('x', shape=(1, 34, 1), maxshape=(None, 34, 1))
       lstmY = lstmF.create_dataset('y', shape=(1,), maxshape=(None,))
-      with h5py.File("VoltNet_2c.h5", 'w') as netF:
+      with h5py.File(net_save, 'w') as netF:
             netX = netF.create_dataset('x', shape=(1, grid_size, 34, 1), maxshape=(None, grid_size, 34, 1))
             netY = netF.create_dataset('y', shape=(1,), maxshape=(None,))
             for fname in f_list:
@@ -46,16 +51,16 @@ with h5py.File("lstm_2c.h5", 'w') as lstmF:
 
 
 
-print("Loading complete")
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range=(-0.5, 0.5))
-for instance in range(data.shape[1]):
-      piece = data[:,instance,:]
-      data[:,instance,:] = scaler.fit_transform(piece)
-data = np.expand_dims(data, axis=-1)
+# print("Loading complete")
+# from sklearn.preprocessing import MinMaxScaler
+# scaler = MinMaxScaler(feature_range=(-0.5, 0.5))
+# for instance in range(data.shape[1]):
+#       piece = data[:,instance,:]
+#       data[:,instance,:] = scaler.fit_transform(piece)
+# data = np.expand_dims(data, axis=-1)
 
-save_fname = "balanced_gird_sensor." + name + ".h5"
-print("Saving to " + save_fname)
-with h5py.File(save_fname,"w") as hf:
-      hf.create_dataset("x", data=data, dtype = 'float32')
-      hf.create_dataset("y", data=tag, dtype = 'float32')
+# save_fname = "balanced_gird_sensor." + name + ".h5"
+# print("Saving to " + save_fname)
+# with h5py.File(save_fname,"w") as hf:
+#       hf.create_dataset("x", data=data, dtype = 'float32')
+#       hf.create_dataset("y", data=tag, dtype = 'float32')
