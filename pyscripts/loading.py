35c02f2713100c78e24a15f7f9d068d6ae1c296d
wornbb <wornbb@gmail.com>
2019-08-10 02:05:11 -0400

    no message

--- pyscripts/loading.py
+++ pyscripts/loading.py
@@ -368,7 +368,10 @@ class training_data_factory():
             vio_count = np.sum(vio_mask)
             norm_count = np.sum(norm_mask)
             print(norm_count)
+            print(vio_count)
+            print(lstm_count)
             new_lstm_count = self.lstm_count + vio_count + norm_count
+            print(new_lstm_count)
             self.lstmX.resize(new_lstm_count, axis=0)
             self.lstmY.resize(new_lstm_count, axis=0)
             self.lstmX[self.lstm_count:self.lstm_count+vio_count,:,0] = buffer[:self.trace-self.pred_str, vio_mask].T
